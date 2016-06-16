5c7308f52e29e603856b52612b098f4f9b260dcc
Josh Hall <joshuadeanhall@gmail.com>
2016-06-15 23:57:30 -0400

    Most of general styling done.  Introduced a datepicker.

--- app/pods/poc/controller.js
+++ app/pods/poc/controller.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   showModal: false,
+  myDate: new Date(),
   actions: {
     toggleModal: function () {
       this.toggleProperty('showModal');
