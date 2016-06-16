f862ccf6559afaf90665b2905122ad6202a1a5c4
Josh Hall <joshuadeanhall@gmail.com>
2016-06-16 11:28:04 -0400

    Base app with validations

--- app/pods/poc/controller.js
+++ app/pods/poc/controller.js
@@ -1,11 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  sideKickValid: true,
   showModal: false,
+  sideKick: '',
   myDate: new Date(),
   actions: {
     toggleModal: function () {
       this.toggleProperty('showModal');
+    },
+    validateSidekick() {
+      if(this.get('sideKick') !== 'Robin') {
+        this.set('sideKickValid', false);
+      }
+      else {
+        this.set('sideKickValid', true);
+      }
     }
   }
 });
