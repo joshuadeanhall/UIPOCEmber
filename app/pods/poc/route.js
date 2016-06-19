d64c10aa0f642f9dfa4a36999601445121305074
Joshua <joshuadeanhall@gmail.com>
2016-06-19 10:21:45 -0400

    Make description editable
    Make description editable

--- app/pods/poc/route.js
+++ app/pods/poc/route.js
@@ -2,6 +2,37 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   isShowingModal: false,
+  model: function() {
+    var model = {
+      records: [{
+        dateStart: '01/01/2016',
+        dateEnd: '',
+        description: 'Simple Ceasar sniffed his sifter',
+        actions: [{
+          description: 'View Request Details'
+        }]
+      },{
+        dateStart: '06/12/15',
+        dateEnd: '06/15/15',
+        description: 'Seized his knees',
+        actions: [{
+          description: 'Cancel Request'
+        },{
+          description: 'View/Edit Request'
+        }]
+      },{
+        dateStart: '04/27/2015',
+        dateEnd: '',
+        description: 'And sneezed',
+        actions: [{
+          description: 'View Request Details'
+        }]
+      }
+      ]
+    };
+    
+    return model;
+  },
   actions: {
     // toggleModal: function () {
     //   this.toggleProperty('isShowingModal');
